b026d96bd5b59e09fac10af4ee844214ea03df49
titaaknt <agnestitania3@gmail.com>
2024-10-29 13:42:34 +0700

    ModifikasiInisialisasiElemenArray01

---
+++ ModifLangkah4Percobaan1.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class ModifLangkah4Percobaan1 {
+    public static void main(String[] args) {
+        int[] bil = {5, 13, -7, 17};
+
+        for (int i = 0; i < 4; i++) {
+            System.out.println(bil[i]);
+        }
+    }
+}
